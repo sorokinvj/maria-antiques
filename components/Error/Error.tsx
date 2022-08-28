d001897245cdc5ca5e0cfd8d68275609c9e165c1
Vladislav Sorokin <sorokinvj@gmail.com>
2022-08-28 13:16:44 +0100

    parseErrorMessage helper

--- components/Error/Error.tsx
+++ components/Error/Error.tsx
@@ -1,18 +1,10 @@
+import { parseErrorMessage } from '@/utils/parseErrorMessage'
+
 interface Props {
   error: unknown
 }
 
 export const ErrorComponent: React.FC<Props> = ({ error }) => {
-  const parseError = (error: unknown) => {
-    if (error instanceof Error) {
-      return error?.message
-    } else if (typeof error === 'string') {
-      return error
-    } else {
-      return JSON.stringify(error)
-    }
-  }
-
   if (!error) return null
-  return <p className="text-red-500">{parseError(error)}</p>
+  return <p className="text-red-500">{parseErrorMessage(error)}</p>
 }
