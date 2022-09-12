5df15616728b2c8a1a8db8b2f03f6c36fba5458a
VV <vashnev@gmail.com>
2022-09-13 00:20:13 +0300

    bounding box for hero image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   images: {
     domains: ['media.graphassets.com']
-  }
+  },
+  experimental: { images: { allowFutureImage: true } }
 }
--- pages/pages/[slug].tsx
+++ pages/pages/[slug].tsx
@@ -2,25 +2,25 @@ import getAllTextPages from '@/lib/get-all-textpages'
 import { getPageData } from '@/lib/get-page-data'
 import getTextPageBySlug from '@/lib/get-text-page-slug'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/future/image'
 import React from 'react'
 import { TextPage } from 'types'
-import Image from 'next/image'
 
 function Page({ textPageData }: { textPageData: TextPage }) {
   return (
     <React.Fragment>
-      <div className="pl-4">
+      <div className="pl-4 md:w-3/4 sm:w-auto">
         {textPageData.page.heroImage && (
-          <Image
-            width={textPageData.page.heroImage.width}
-            height={textPageData.page.heroImage.height}
-            alt={textPageData.title}
-            src={textPageData.page.heroImage.url}
-          />
+            <Image
+              width={textPageData.page.heroImage.width}
+              height={textPageData.page.heroImage.height}
+              alt={textPageData.title}
+              src={textPageData.page.heroImage.url}
+              className=" sm:h-auto md:h-96 object-cover"
+            />
         )}
 
         <div
-          className="md:w-3/4 sm:w-auto"
           dangerouslySetInnerHTML={{ __html: textPageData.page.content.html }}
         />
       </div>
