c456d14218da10d549e4194a272f9b8cf560bd01
Vladislav Sorokin <sorokinvj@gmail.com>
2022-09-22 22:05:42 +0100

    fix total in order creation

--- lib/create-order.ts
+++ lib/create-order.ts
@@ -1,5 +1,6 @@
 import hygraphMutationClient, { gql } from '@/lib/hygraph-mutation-client'
 import stripe from '@/lib/stripe-client'
+import { convertPriceFormat } from '@/utils/convert-price-format'
 import { getShippingCost } from '@/utils/getShippingCost'
 import { parseCountry } from '@/utils/parseCountry'
 import { parseErrorMessage } from '@/utils/parseErrorMessage'
@@ -26,35 +27,32 @@ export async function createOrder({ sessionId }: { sessionId: string }) {
   } = session
 
   try {
-    const cmsResponse = await hygraphMutationClient.request(
-      createOrderMutation,
-      {
-        order: {
-          email: customer_details?.email,
-          total: amount_total,
-          stripeCheckoutId: id,
-          orderItems: {
-            create: line_items?.data.map((item: any) => ({
-              total: item.amount_total,
-              product: {
-                connect: {
-                  id: item.price.product.metadata.productId
-                }
+    const orderId = await hygraphMutationClient.request(createOrderMutation, {
+      order: {
+        email: customer_details?.email,
+        total: convertPriceFormat('stripeToCms', amount_total!),
+        stripeCheckoutId: id,
+        orderItems: {
+          create: line_items?.data.map((item: any) => ({
+            total: item.amount_total,
+            product: {
+              connect: {
+                id: item.price.product.metadata.productId
               }
-            }))
-          },
-          shippingInfo: {
-            ...shipping_details,
-            address: {
-              ...shipping_details?.address,
-              country: parseCountry(shipping_details?.address?.country)
             }
-          },
-          shippingCost: getShippingCost(shipping_cost)
-        }
+          }))
+        },
+        shippingInfo: {
+          ...shipping_details,
+          address: {
+            ...shipping_details?.address,
+            country: parseCountry(shipping_details?.address?.country)
+          }
+        },
+        shippingCost: getShippingCost(shipping_cost)
       }
-    )
-    return cmsResponse
+    })
+    return orderId
   } catch (error) {
     console.error('Error on creating order', parseErrorMessage(error))
     throw error
