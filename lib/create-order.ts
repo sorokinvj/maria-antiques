2ab1c2c978c30421cbf8cc5b015fa004e58824ef
Vladislav Sorokin <sorokinvj@gmail.com>
2022-10-28 21:14:37 +0100

    fix orderId

--- lib/create-order.ts
+++ lib/create-order.ts
@@ -26,36 +26,33 @@ export async function createOrder(
   } = session
 
   try {
-    const cmsResponse = await hygraphMutationClient.request(
-      createOrderMutation,
-      {
-        order: {
-          email: customer_details?.email,
-          total: convertPriceFormat('stripeToCms', amount_total || 0),
-          stripeCheckoutId: id,
-          orderItems: {
-            create: line_items?.data.map((item: any) => ({
-              price: convertPriceFormat('stripeToCms', item.amount_subtotal),
-              tax: convertPriceFormat('stripeToCms', item.amount_tax),
-              product: {
-                connect: {
-                  id: item.price.product.metadata.productId
-                }
+    const orderId = await hygraphMutationClient.request(createOrderMutation, {
+      order: {
+        email: customer_details?.email,
+        total: convertPriceFormat('stripeToCms', amount_total || 0),
+        stripeCheckoutId: id,
+        orderItems: {
+          create: line_items?.data.map((item: any) => ({
+            price: convertPriceFormat('stripeToCms', item.amount_subtotal),
+            tax: convertPriceFormat('stripeToCms', item.amount_tax),
+            product: {
+              connect: {
+                id: item.price.product.metadata.productId
               }
-            }))
-          },
-          shippingInfo: {
-            ...shipping_details,
-            address: {
-              ...shipping_details?.address,
-              country: parseCountry(shipping_details?.address?.country)
             }
-          },
-          shippingCost: getShippingCost(shipping_cost)
-        }
+          }))
+        },
+        shippingInfo: {
+          ...shipping_details,
+          address: {
+            ...shipping_details?.address,
+            country: parseCountry(shipping_details?.address?.country)
+          }
+        },
+        shippingCost: getShippingCost(shipping_cost)
       }
-    )
-    return cmsResponse
+    })
+    return orderId
   } catch (error) {
     console.error('Error on creating order', parseErrorMessage(error))
     throw error
